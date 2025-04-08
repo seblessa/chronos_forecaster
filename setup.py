8c2aaaf6b4f24807f542e84a71a725639e9a8738
Sebastião Santos Lessa <93839108+seblessa@users.noreply.github.com>
2025-04-08 15:23:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chronos_forecaster",
-    version="0.1.3",
+    version="0.1.4",
     description="Making time series forecasting with Amazon's Foundation Model Chronos simple and accessible.",
     author="Sebastião Santos Lessa",
     author_email="sebastiao.lessa@inesctec.pt",
